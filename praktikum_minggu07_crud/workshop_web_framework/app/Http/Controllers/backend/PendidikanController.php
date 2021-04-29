d53ce5abce0a09ca91429ef78b7201d4eda65eba
Ulfiatun Hasanah <ulfiezafiekhasanah008@gmail.com>
2021-04-29 10:23:57 +0700

    Membuat CRUD

---
+++ praktikum_minggu07_crud/workshop_web_framework/app/Http/Controllers/backend/PendidikanController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use Illuminate\Http\Request;
+
+class PendidikanController extends Controller
+{
+    //
+}
---
+++ praktikum_minggu07_crud/workshop_web_framework/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//use App\Http\Controllers\ManagementUserController;
+
+//use App\Http\Controllers\frontend\HomeController;
+
+use App\Http\Controllers\backend\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('user', [ManagementUserController::class, 'index']);
+//Route::resource('user', ManagementUserController::class);
+
+Route::get("/home", function () {
+    return view("home");
+});
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*Route::group(['namespace' => 'App\Http\Controllers\frontend'], function () {
+    Route::resource('home', 'HomeController');
+});*/
+
+Route::group(['namespace' => 'App\Http\Controllers\backend'], function () {
+    Route::resource('dashboard', 'DashboardController');
+    Route::resource('pendidikan', 'PendidikanController');
+    Route::resource('pengalaman_kerja', 'PengalamanKerjaController');
+});
